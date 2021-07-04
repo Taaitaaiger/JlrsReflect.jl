e0279537c9dd2c2705ba99503cb177fe9e6fbc47
Thomas van Doornmalen <thomas.vandoornmalen@gmail.com>
2021-07-04 19:36:50 +0200

    Add tests for zero-sized types

---
+++ test/ZeroSized.jl
@@ -0,0 +1,29 @@
+struct Empty end
+
+struct TypedEmpty{T} end
+
+@testset "Struct with no fields" begin
+    @test begin
+        b = JlrsReflect.reflect([Empty])
+        sb = JlrsReflect.StringWrappers(b)
+
+        sb[Empty] === """#[repr(C)]
+        #[derive(Clone, Debug, Unbox, ValidLayout, Typecheck, IntoJulia)]
+        #[jlrs(julia_type = "Main.Empty", zero_sized_type)]
+        pub struct Empty {
+        }"""
+    end
+end
+
+@testset "Struct with type parameter but no fields" begin
+    @test begin
+        b = JlrsReflect.reflect([TypedEmpty])
+        sb = JlrsReflect.StringWrappers(b)
+
+        sb[JlrsReflect.basetype(TypedEmpty)] === """#[repr(C)]
+        #[derive(Clone, Debug, Unbox, ValidLayout, Typecheck)]
+        #[jlrs(julia_type = "Main.TypedEmpty")]
+        pub struct TypedEmpty {
+        }"""
+    end
+end
